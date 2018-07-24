ddebda91aca75293a27ff896df4858e75f5eece4
Akigaze <1175520196@qq.com>
2018-07-24 23:20:02 +0800

    [Akigaze] add getEmployee API of EmployeeController.java

--- src/main/java/com/oocl/company/service/EmployeeService.java
+++ src/main/java/com/oocl/company/service/EmployeeService.java
@@ -12,12 +12,15 @@ public class EmployeeService {
     private List<Employee> employees;
 
     {
-        employees=new ArrayList<>();
+        this.employees=new ArrayList<>();
         employees.add(new Employee(1,"Mark",22,"man",500));
         employees.add(new Employee(2,"Bale",21,"man",500));
         employees.add(new Employee(3,"Tracy",20,"woman",500));
         employees.add(new Employee(4,"Leo",22,"man",500));
         employees.add(new Employee(5,"Quinn",21,"woman",500));
+    }
+
+    public EmployeeService() {
     }
 
     public EmployeeService(List<Employee> employees) {
