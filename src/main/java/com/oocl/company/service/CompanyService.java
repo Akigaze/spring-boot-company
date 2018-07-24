aaf6331b0179e0e824bf5e5deeb03d3a225c2cdc
Akigaze <1175520196@qq.com>
2018-07-25 00:31:18 +0800

    [Akigaze] add  CompanyService.java

---
+++ src/main/java/com/oocl/company/service/CompanyService.java
@@ -0,0 +1,27 @@
+package com.oocl.company.service;
+
+import com.oocl.company.model.Company;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CompanyService {
+
+    private List<Company> companies;
+
+    public CompanyService() {
+    }
+
+    public CompanyService(List<Company> companies) {
+        this.companies = companies;
+    }
+
+    public List<Company> getCompanies() {
+        return companies;
+    }
+
+    public void setCompanies(List<Company> companies) {
+        this.companies = companies;
+    }
+}
