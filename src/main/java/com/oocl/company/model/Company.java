4b77a893baf0ce6a545c395c1ea314e9421dbd6c
Akigaze <1175520196@qq.com>
2018-07-25 00:29:47 +0800

    [Akigaze] add model Comany.java

---
+++ src/main/java/com/oocl/company/model/Company.java
@@ -0,0 +1,48 @@
+package com.oocl.company.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class Company {
+    private String companyName;
+    private int employeeNumber;
+
+    @Autowired
+    private List<Employee> employees;
+
+    public Company() {
+    }
+
+    public Company(String companyName, int employeeNumber, List<Employee> employees) {
+        this.companyName = companyName;
+        this.employeeNumber = employeeNumber;
+        this.employees = employees;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public int getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public void setEmployeeNumber(int employeeNumber) {
+        this.employeeNumber = employeeNumber;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+}
